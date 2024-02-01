34ab711567b58a07f046e9f0653ffe959907f82e
PaulaLuana2 <paulaluanamiranda@gmail.com>
2024-02-01 11:15:32 -0300

    home page styling

--- listatarefas/src/pages/Admin/index.js
+++ listatarefas/src/pages/Admin/index.js
@@ -1,9 +1,46 @@
+import { useState } from 'react';
 import './admin.css';
 
+import { auth } from '../../connection'
+import {signOut} from 'firebase/auth'
+
 export default function Admin(){
+    const [tarefaInput, setTarefaInput] = useState('');
+
+    function handleRegister(e){
+        e.preventDefault();
+        alert('Clicou!')
+    }
+
+    async function handleLogout(){
+        await signOut(auth);
+    }
+
     return(
-        <div>
-            <h1>Página ADMIN</h1>
+        <div className='admin-container'>
+            <h1>Minhas tarefas</h1>
+
+            <form className='form' onSubmit={handleRegister}>
+                <textarea
+                placeholder='Digite sua tarefa'
+                value={tarefaInput}
+                onChange={(e) => setTarefaInput(e.target.value)}
+                />
+
+                <button className='btn-register' type='submit'>Registrar tarefa</button>
+            </form>
+
+            <article className='list'>
+                <p>Estudaaaar</p>
+
+                <div>
+                    <button>Editar</button>
+                    <button className='btn-delete'>Concluir</button>
+                </div>
+            </article>
+
+
+            <button className='btn-logout' onClick={handleLogout}>Sair</button>
         </div>
     )
 }
