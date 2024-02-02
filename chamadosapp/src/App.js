07c61033af25d8ce56d767a752a29c222ca0e7f0
PaulaLuana2 <paulaluanamiranda@gmail.com>
2024-02-02 11:58:45 -0300

    feature:user-registration

--- chamadosapp/src/App.js
+++ chamadosapp/src/App.js
@@ -1,10 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import RoutesApp from "./routes";
 
+import AuthProvider from "./contexts/auth";
+import { ToastContainer } from "react-toastify"; //config p/ usar o toast
+import 'react-toastify/dist/ReactToastify.css'; //config p/ usar o toast
+
 function App() {
   return (
     <BrowserRouter>
-      <RoutesApp/>
+      <AuthProvider>
+        <ToastContainer autoClose={3000}/>
+        <RoutesApp/>
+        </AuthProvider>
     </BrowserRouter>
   );
 }
---
+++ chamadosapp/src/contexts/auth.js
@@ -0,0 +1,72 @@
+import { useState, createContext, useEffect } from "react";
+import { auth, db } from '../services/firebaseConnection';
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+export const AuthContext = createContext({});
+
+
+function AuthProvider({ children }){
+    const [user, setUser] = useState(null);
+    const [loadingAuth, setLoadingAuth] = useState(false);
+
+    const navigate = useNavigate();
+
+    async function signUp(email, password, name){
+        setLoadingAuth(true);
+
+        await createUserWithEmailAndPassword(auth, email, password)
+        .then(async(value) => {
+            let uid = value.user.uid;
+
+            await setDoc(doc(db, "users", uid), {
+                nome: name,
+                avatarUrl: null,
+            })
+            .then(() => {
+                let data = {
+                    uid: uid,
+                    nome: name,
+                    email: value.user.email,
+                    avatarUrl: null
+                };
+
+                setUser(data);
+                storageUser(data);
+                setLoadingAuth(false);
+                toast.success("Seja bem vindo ao sistema!")
+                navigate("/dashboard");
+            })
+            .catch((error) => {
+                console.log(error);
+                setLoadingAuth(false);
+            })
+        })
+    }
+
+    function signIn(email, password, name){
+
+    }
+
+    function storageUser(data){
+        localStorage.setItem('@tickesPRO', JSON.stringify(data));
+    }
+
+    return(
+        <AuthContext.Provider 
+        value={{
+            signed: !!user,
+            user,
+            signIn,
+            signUp,
+            loadingAuth
+        }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
