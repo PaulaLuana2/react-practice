5d7cc7f53a55001980fae9915f5b5e05adb2bea7
PaulaLuana2 <paulaluanamiranda@gmail.com>
2024-02-06 12:09:00 -0300

    feature: pagination

--- chamadosapp/src/pages/Dashboard/index.js
+++ chamadosapp/src/pages/Dashboard/index.js
@@ -1,5 +1,4 @@
-import { useContext, useEffect, useState } from 'react';
-import { AuthContext} from '../../contexts/auth';
+import { useEffect, useState } from 'react';
 
 import Header from '../../components/Header';
 import Title from '../../components/Title';
@@ -15,12 +14,13 @@ import { db } from '../../services/firebaseConnection';
 const listRef = collection(db, "chamados");
 
 export default function Dashboard(){
-    const { logout } = useContext(AuthContext);
-
     const [chamados, setChamados] = useState([]);
     const [loading, setLoading] = useState(true);
+    
     const [isEmpty, setIsEmpty] = useState(false);
-
+    const [lastDocs, setLastDocs] = useState();
+    const [loadingMore, setLoadingMore] = useState(false);
+    
     useEffect (() => {
         async function loadChamados(){
             const q = query(listRef, orderBy('created', 'desc'), limit(5));
@@ -55,10 +55,24 @@ export default function Dashboard(){
                 })
             });
 
+            const lastDoc = querySnapshot.docs[querySnapshot.docs.length - 1];
+
             setChamados(chamados => [...chamados, ...lista]);
+            setLastDocs(lastDoc);
+
         }else{
             setIsEmpty(true);
         }
+
+        setLoadingMore(false);
+    }
+
+    async function handleMore(){
+        setLoadingMore(true);
+
+        const q = query(listRef, orderBy('created', 'desc'), startAfter(lastDocs), limit(5));
+        const querySnapshot = await getDocs(q);
+        await updateState(querySnapshot);
     }
 
     if(loading){
@@ -114,7 +128,7 @@ export default function Dashboard(){
                                             <td data-label="Cliente">{item.cliente}</td>
                                             <td data-label="Assunto">{item.assunto}</td>
                                             <td data-label="Status">
-                                                <span className='badge' style={{backgroundColor: '#999'}}>
+                                                <span className='badge' style={{backgroundColor: item.status === 'Aberto' ? '#5cb85c' : '#999'}}>
                                                     {item.status}
                                                 </span>
                                             </td>
@@ -132,6 +146,10 @@ export default function Dashboard(){
                                 })}
                             </tbody>
                         </table>
+
+
+                        {loadingMore && <h3>Buscando mais chamados...</h3>}
+                        {!loadingMore && !isEmpty && <button className="btn-more" onClick={handleMore}>Buscar mais</button>}
                         </>
                     )}
                 </>
